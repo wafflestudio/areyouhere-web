b0a098f2491f5bed6c726ea713f1aa04e2535e43
jwchoi-kr <jwchoi.tech@gmail.com>
2024-04-13 22:56:27 +0900

    fix: change date format when editting

--- src/components/sessions/SessionAttendeeItem.tsx
+++ src/components/sessions/SessionAttendeeItem.tsx
@@ -45,9 +45,7 @@ function SessionAttendeeItem({
         />
       </TableItem>
       <TableItem to={to}>
-        {!isEditing && attendanceStatus
-          ? dateFormat(attendanceTime, "HH:MM:ss")
-          : "--:--:--"}
+        {attendanceStatus ? dateFormat(attendanceTime, "HH:MM:ss") : "--:--:--"}
       </TableItem>
     </tr>
   );
