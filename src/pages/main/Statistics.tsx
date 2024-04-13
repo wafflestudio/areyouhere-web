f93a8cefa278e2f9546fbbc9ce588138db97e19c
jwchoi-kr <jwchoi.tech@gmail.com>
2024-04-13 23:39:36 +0900

    feat: add coming soon on statistics

--- src/pages/main/Statistics.tsx
+++ src/pages/main/Statistics.tsx
@@ -6,6 +6,11 @@ function Statistics() {
   return (
     <Container>
       <TitleBar label="Statistics" />
+      <ContentContainer>
+        <h3>Coming soon</h3>
+        <p>We’re currently working hard on this page.</p>
+        <p>Stay tuned for update!</p>
+      </ContentContainer>
     </Container>
   );
 }
@@ -15,6 +20,24 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+`;
+
+const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 20rem;
+
+  & h3 {
+    ${({ theme }) => theme.typography.h3};
+    color: ${({ theme }) => theme.colors.primary[500]};
+
+    margin-bottom: 0.8rem;
+  }
+
+  & p {
+    ${({ theme }) => theme.typography.b1};
+  }
 `;
 
 export default Statistics;
