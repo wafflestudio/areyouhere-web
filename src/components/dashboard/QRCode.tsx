54625ec9896c0101d242f2e40d7b4521c841d6e6
Scripter36 <1350adwx@gmail.com>
2025-02-08 15:13:27 +0900

    feat: enhance QR code styling and layout in code popup

--- src/components/dashboard/QRCode.tsx
+++ src/components/dashboard/QRCode.tsx
@@ -1,28 +1,48 @@
-import { QRCodeCanvas } from "qrcode.react";
-import styled from "styled-components";
+import QRCodeStyling from "qr-code-styling";
+import { useEffect, useRef } from "react";
+import styled, { useTheme } from "styled-components";
 
 function QRCodeGenerator({ passcode }: { passcode: string | undefined }) {
+  const qrCodeRef = useRef<HTMLDivElement>(null);
+  const theme = useTheme();
   const baseUrl = "https://areyouhere.today";
   const qrUrl = `${baseUrl}?code=${passcode}`;
 
-  return (
-    <Container>
-      <QRCodeCanvas value={qrUrl} size={256} />
-      <p>Scan this QR code for attendance</p>
-    </Container>
-  );
+  useEffect(() => {
+    if (qrCodeRef.current) {
+      // Clear previous QR code
+      qrCodeRef.current.innerHTML = "";
+
+      const qrCode = new QRCodeStyling({
+        width: 256,
+        height: 256,
+        data: qrUrl,
+        dotsOptions: {
+          color: theme.colors.primary[500],
+          type: "dots",
+        },
+
+        cornersDotOptions: {
+          type: "extra-rounded",
+        },
+        cornersSquareOptions: {
+          type: "extra-rounded",
+        },
+        backgroundOptions: {
+          color: theme.colors.white,
+        },
+      });
+      qrCode.append(qrCodeRef.current);
+    }
+  }, [qrUrl, theme]);
+
+  return <Container ref={qrCodeRef} />;
 }
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 1rem;
-
-  p {
-    font-size: 1.7rem;
-    color: ${({ theme }) => theme.colors.darkGrey};
-    font-weight: 500;
+  > canvas {
+    width: 100%;
+    height: 100%;
   }
 `;
 
